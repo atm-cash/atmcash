ac1487ebd6e2966229127a5176574239925ac26e
atm-cash <year74@gmail.com>
2026-05-27 20:46:18 +0200

    Delete src/components/CurrencyConverter/CurrencyConverter.jsx

--- src/components/CurrencyConverter/CurrencyConverter.jsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import CurrencyInput from "./CurrencyInput";
-function CurrencyConverter() { return (<div><h2 className="text-lg font-semibold mb-2">Valutaomregner</h2><CurrencyInput /></div>); }
-export default CurrencyConverter;
